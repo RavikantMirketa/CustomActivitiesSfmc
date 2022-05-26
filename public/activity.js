826f12b810c107ae37c053e45e7f45befaf3aca1
RavikantMirketa <ravikant@mirketa.com>
2022-05-26 17:50:24 +0530

    Blog Updated Changes

--- public/activity.js
+++ public/activity.js
@@ -1,14 +1,55 @@
 var connection = new Postmonger.Session();
 
+const activityJson = {
+  name: '',
+  id: null,
+  key: 'REST-1',
+  arguments: {
+    execute: {
+      inArguments: [
+        {
+          contactKey: '{{Contact.Key}}',
+        },
+        {
+          FirstName: '{{Contact.Attribute.Categories.FirstName}}',
+        },
+        {
+          LastName: '{{Contact.Attribute.Categories.LastName}}',
+        }
+      ],
+      url: 'https://requestbin.fullcontact.com/r92a9gr9/execute',
+    },
+  },
+  configurationArguments: {
+    publish: {
+      url: 'https://requestbin.fullcontact.com/r92a9gr9/publish',
+    },
+  },
+  metaData: {
+    icon: 'https://sample-activity.herokuapp.com/icon.png',
+    category: 'message',
+    iconSmall: null,
+    original_icon: 'icon.png',
+    isConfigured: true,
+  },
+  editable: true,
+  outcomes: [
+    {
+      next: 'WAITBYDURATION-1',
+    },
+  ],
+  errors: [],
+}; 
+ 
 // Startup Sequence
 connection.trigger('ready');
 
-connection.on('initActivity', function( data ) {
-  document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
+connection.on('initActivity', function (data) {
+  //document.getElementById( 'configuration' ).value = JSON.stringify(activityJson, null, 2 );
 });
 
 // Save Sequence
-connection.on('clickedNext', function() {
-  var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
+connection.on('clickedNext', function () {
+  var configuration = activityJson; 
   connection.trigger('updateActivity', configuration);
-});
+}); 
